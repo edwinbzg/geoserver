513a52c2d47c6872090fccf3603b9add53f9a150
Edwin Bautista <97236733+edwinbzg@users.noreply.github.com>
2022-01-26 08:44:00 -0500

    Add rm backup

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Upload backup data_dir"
-gsutil -m cp -r /opt/geoserver/data_dir gs://geomanguera/data_dir
+gsutil rm -r gs://geomanguera/data_dir && gsutil -m cp -r /opt/geoserver/data_dir gs://geomanguera
 echo "Backup data_dir complete"
