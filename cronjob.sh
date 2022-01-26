ad63e82aaf54ca69cf26610e91aca45e34f4d6d6
Edwin Bautista <97236733+edwinbzg@users.noreply.github.com>
2022-01-26 08:44:32 -0500

    Every 30 mins

--- cronjob.sh
+++ cronjob.sh
@@ -1,1 +1,8 @@
-* 1 * * * ./backup.sh
+#!/bin/bash
+echo "Crontab install......."
+
+echo "*/30 * * * * /app/backup.sh >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
